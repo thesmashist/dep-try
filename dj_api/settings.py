37cfa079bef6bb295c98044756120da16181a788
jkamzy254 <71163468+jkamzy254@users.noreply.github.com>
2023-06-05 17:04:16 +1000

    Update settings.py

--- dj_api/settings.py
+++ dj_api/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'dj_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'sql_server.pyodbc',
+        'ENGINE': 'mssql',
         'NAME': 'D3_DEV',
         'USER': 'd3admin@d3aumelb.database.windows.net',
         'PASSWORD': 'St33lS0ld13rs',
