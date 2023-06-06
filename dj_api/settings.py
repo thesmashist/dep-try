983fd3393983a0e7482fa0462dbff0f43d925a38
Dilan Horana <dilan.horana@aroflo.com>
2023-06-06 21:17:32 +1000

    mimetype for js file

--- dj_api/settings.py
+++ dj_api/settings.py
@@ -15,6 +15,7 @@ import os
 import pymssql
 import mimetypes
 mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("text/javascript", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
