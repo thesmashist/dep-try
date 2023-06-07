cafe0c0ff2d2f1a500c1f2652405d6445d1e6030
Dilan Horana <dilan.horana@aroflo.com>
2023-06-07 20:26:48 +1000

    update urls

--- dj_api/urls.py
+++ dj_api/urls.py
@@ -17,14 +17,21 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic.base import TemplateView
 from rest_framework_simplejwt import views as jwt_views
+from django.urls import path
+from base.api import views
+from base.api.views import MyTokenObtainPairView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
 	# path('', include('app.urls')),
 	path('api/', include('users.urls')),
